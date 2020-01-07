6057914b0d0c3ce1f455fb6a022f572321018bcf
Mikola161 <nlryzhakov@sberbank.ru>
2020-01-07 16:42:12 +0300

    Create class Person

---
+++ .idea/src/Person.java
@@ -0,0 +1,53 @@
+public class Person {
+    private final boolean man;
+    private final String name;
+    private Person spouse;
+    public Person(boolean man, String name) {
+        this.man = man;
+        this.name = name;
+    }
+
+    /**
+     * This method checks gender of persons. If genders are not equal - tries to marry.
+     * If one of them has another spouse - execute divorce(sets spouse = null for husband and wife. Example: if both persons have spouses - then divorce will set 4 spouse to null) and then executes marry().
+     * @param person - new husband/wife for this person.
+     * @return - returns true if this person has another gender than passed person and they are not husband and wife, false otherwise
+     */
+    public boolean marry(Person person) {
+        if (person.man != man){
+            if (spouse == null) {
+                spouse = person;
+                return true;
+            }
+            else {
+                if (spouse.equals(person)){
+                    return true;
+                }
+                else {
+                    person.divorce();
+                    this.divorce();
+                    spouse = person;
+                    person.marry(this);
+                    return true;
+                }
+            }
+        }
+        else return false;
+    }
+
+    /**
+     * Sets spouse = null if spouse is not null
+     * @return true - if person status has been changed
+     */
+    public boolean divorce() {
+        Person tmpPerson = spouse;
+        if (spouse != null){
+            spouse = null;
+            tmpPerson.divorce();
+            return true;
+        }
+        else return false;
+    }
+
+
+}
